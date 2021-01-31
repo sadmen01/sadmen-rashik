9a4163f42b41b7c7ad84436048f1d5e4d2581c6c
sadmen01 <60668870+sadmen01@users.noreply.github.com>
2021-01-31 18:23:34 +0600

    initial commit

---
+++ jgv/package main.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+
+
+fun main(){
+
+
+
+fmt.println(" My name is Mohammed Sademn Rashik Khan")
+fmt.println("I live in Bangladesh")
+fmt.println(" I'm just going to finish my graduation from a privet university")
+fmt.println("right now i'm looking for a job as a software engenner")
+
+
+
+}
